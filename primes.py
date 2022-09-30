b0fcd69b4719ff90b7d572eac1eef43e2ca875ad
Zahid <k21043022@nmslab-004151.nms.kcl.ac.uk>
2022-09-30 15:10:26 +0100

    Updated Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,36 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+
+    def isprime(num):
+        for n in range(2,int(num**0.5)+1):
+
+            if num%n==0:
+                return False
+        return True
+
+
+
     list = []
+
+
+    if number_of_primes == 0 or number_of_primes < 0:
+        return ValueError
+
+
+    for x in range (2, number_of_primes):
+
+
+        if isprime(x) == True:
+            list.append(x)
+
+        else:
+            pass
+
+       
+
+        
+
     return list
+
